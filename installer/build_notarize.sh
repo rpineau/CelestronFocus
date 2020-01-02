ef9cb361a2d0a2d9d17f3a9f1d153bffbd2a21b4
Rodolphe Pineau <pineau@rti-zone.org>
2020-01-02 08:41:20 -0800

    Fix build scripts

--- installer/build_notarize.sh
+++ installer/build_notarize.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-mkdir -p ROOT/tmp/CelestronFocus_X2/
-cp "../CelestronFocus.ui" ROOT/tmp/CelestronFocus_X2/
-cp "../focuserlist CelestronFocus.txt" ROOT/tmp/CelestronFocus_X2/
-cp "../build/Release/libCelestronFocus.dylib" ROOT/tmp/CelestronFocus_X2/
-
 PACKAGE_NAME="CelestronFocus_X2.pkg"
 BUNDLE_NAME="org.rti-zone.CelestronFocusX2"
 if [ ! -z "$app_id_signature" ]; then
     codesign -s "$app_id_signature" ../build/Release/libCelestronFocus.dylib
 fi
+
+mkdir -p ROOT/tmp/CelestronFocus_X2/
+cp "../CelestronFocus.ui" ROOT/tmp/CelestronFocus_X2/
+cp "../focuserlist CelestronFocus.txt" ROOT/tmp/CelestronFocus_X2/
+cp "../build/Release/libCelestronFocus.dylib" ROOT/tmp/CelestronFocus_X2/
 
 if [ ! -z "$installer_signature" ]; then
 	# signed package using env variable installer_signature
